bd507bc392fce9bc3f53de906a0fa066ca33bcff
faranak-cs <fakhan.cs@gmail.com>
2023-12-30 15:50:49 +0000

    Added File Storage in PortfolioViewModel

---
+++ EquityX/EquityX.Maui/FileHandler/StorageManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+
+namespace EquityX.Maui.FileHandler;
+
+public class StorageManager
+{
+    /// <summary>
+    /// STORE DATA
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="filePath"></param>
+    /// <param name="data"></param>
+    public static void StoreToFile<T>(string filePath, T data)
+    {
+        try
+        {
+            string json = JsonSerializer.Serialize(data);
+            File.WriteAllText(filePath, json);
+        }
+        catch (Exception ex)
+        {
+            // left for now
+            Console.WriteLine($"Failed to serialize data: {ex.Message}", ex);
+        }
+    }
+
+    /// <summary>
+    /// LOAD DATA
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public static T LoadFromFile<T>(string filePath)
+    {
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<T>(json);
+            }
+        }
+        catch (Exception ex)
+        {
+            // left for now
+            Console.WriteLine($"Failed to deserialize data: {ex.Message}", ex);
+        }
+        return default(T);
+    }
+
+    public static string GetFilePath(string fileName)
+    {
+        return Path.Combine(FileSystem.AppDataDirectory, fileName);
+    }
+}
--- EquityX/EquityX.Maui/ViewModels/PortfolioPageViewModel.cs
+++ EquityX/EquityX.Maui/ViewModels/PortfolioPageViewModel.cs
@@ -1,141 +1,179 @@
-﻿using EquityX.Maui.Models;
+﻿using EquityX.Maui.FileHandler;
+using EquityX.Maui.Models;
 
-namespace EquityX.Maui.ViewModels
+namespace EquityX.Maui.ViewModels;
+
+
+//static repo for mockups/fake data
+public static class PortfolioPageViewModel
 {
+    /// <summary>
+    /// LIST FOR PORTFOLIO PAGE
+    /// </summary>
+    public static List<Assets> _assets = new List<Assets>();
 
-    //static repo for mockups/fake data
-    public static class PortfolioPageViewModel
+    // FILE PATH "ASSETS.JSON"
+    private static string path = StorageManager.GetFilePath("assets.json");
+
+    // STORE DATA INTO FILE
+    private static void StoreAssets()
     {
-        /// <summary>
-        /// LIST FOR PORTFOLIO PAGE
-        /// </summary>
-        public static List<Assets> _assets = new List<Assets>();
+        StorageManager.StoreToFile(path, _assets);
+    }
 
-        /// <summary>
-        /// GET ASSETS
-        /// </summary>
-        /// <returns></returns>
-        public static List<Assets> GetAssets() => _assets;
+    // LOAD DATA FROM FILE
+    private static void LoadAssets()
+    {
+        var Assets = StorageManager.LoadFromFile<List<Assets>>(path);
 
-        /// <summary>
-        /// TOTAL ASSET VALUE
-        /// </summary>
-        /// <returns></returns>
-        public static double GetSum()
+        if (Assets != null)
         {
-            double sum = 0;
-            foreach (var asset in _assets)
-            {
-                sum = sum + asset.Investment;
+            _assets = Assets;
+        }
+    }
 
-            }
-            return sum;
+    /// <summary>
+    /// GET ASSETS
+    /// </summary>
+    /// <returns></returns>
+    public static List<Assets> GetAssets()
+    {
+        LoadAssets();
+
+        return _assets;
+    }
+
+    /// <summary>
+    /// TOTAL ASSET VALUE
+    /// </summary>
+    /// <returns></returns>
+    public static double GetSum()
+    {
+        LoadAssets();
+
+        double sum = 0;
+        foreach (var asset in _assets)
+        {
+            sum = sum + asset.Investment;
+
+        }
+        return sum;
+    }
+
+
+    /// <summary>
+    /// GET ASSET BY ID
+    /// </summary>
+    /// <param name="assetId"></param>
+    /// <returns></returns>
+    public static Assets GetAssetById(int assetId)
+    {
+        LoadAssets();
+        var asset = _assets.FirstOrDefault(x => x.AssetId == assetId);
+
+        if (asset != null)
+        {
+            return new Assets
+            {
+                AssetId = asset.AssetId,
+                Name = asset.Name,
+                Investment = asset.Investment,
+                Unit = asset.Unit,
+                Summary = asset.Summary
+            };
         }
 
+        return null;
+    }
 
-        /// <summary>
-        /// GET ASSET BY ID
-        /// </summary>
-        /// <param name="assetId"></param>
-        /// <returns></returns>
-        public static Assets GetAssetById(int assetId)
+    /// <summary>
+    /// GET ASSET BY NAME
+    /// </summary>
+    /// <param name="assetName"></param>
+    /// <returns></returns>
+    public static Assets GetAssetByName(string assetName)
+    {
+        LoadAssets();
+        var asset = _assets.FirstOrDefault(x => x.Name == assetName);
+
+        if (asset != null)
         {
-            var asset = _assets.FirstOrDefault(x => x.AssetId == assetId);
-
-            if (asset != null)
+            return new Assets
             {
-                return new Assets
-                {
-                    AssetId = asset.AssetId,
-                    Name = asset.Name,
-                    Investment = asset.Investment,
-                    Unit = asset.Unit,
-                    Summary = asset.Summary
-                };
-            }
+                AssetId = asset.AssetId,
+                Name = asset.Name,
+                Investment = asset.Investment,
+                Unit = asset.Unit,
+                // CHECK ?
+                Summary = asset.Summary,
 
-            return null;
+            };
         }
 
-        /// <summary>
-        /// GET ASSET BY NAME
-        /// </summary>
-        /// <param name="assetName"></param>
-        /// <returns></returns>
-        public static Assets GetAssetByName(string assetName)
+        return null;
+    }
+
+    /// <summary>
+    /// THIS WILL HANDLE THE LOGIC TO ADD THE ASSET
+    /// </summary>
+    /// <param name="asset"></param>
+    public static void AddAsset(Assets asset)
+    {
+        LoadAssets();
+
+
+        // PORTFOLIO PAGE
+        var existingAsset = _assets.FirstOrDefault(x => x.Name == asset.Name);
+        if (existingAsset == null)
         {
-            var asset = _assets.FirstOrDefault(x => x.Name == assetName);
-
-            if (asset != null)
+            // STOCK DOES NOT EXIST IN THE LIST
+            if (_assets.Count == 0)
             {
-                return new Assets
-                {
-                    AssetId = asset.AssetId,
-                    Name = asset.Name,
-                    Investment = asset.Investment,
-                    Unit = asset.Unit,
-                    // CHECK ?
-                    Summary = asset.Summary,
-
-                };
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// THIS WILL HANDLE THE LOGIC TO ADD THE ASSET
-        /// </summary>
-        /// <param name="asset"></param>
-        public static void AddAsset(Assets asset)
-        {
-            // PORTFOLIO PAGE
-            var existingAsset = _assets.FirstOrDefault(x => x.Name == asset.Name);
-            if (existingAsset == null)
-            {
-                // STOCK DOES NOT EXIST IN THE LIST
-                if (_assets.Count == 0)
-                {
-                    // LIST IS EMPTY
-                    asset.AssetId = 0;
-                    _assets.Add(asset);
-                }
-                else
-                {
-                    // LIST IS NOT EMPTY
-                    asset.AssetId = (_assets.Max(x => x.AssetId)) + 1;
-                    _assets.Add(asset);
-                }
+                // LIST IS EMPTY
+                asset.AssetId = 0;
+                _assets.Add(asset);
             }
             else
             {
-                // STOCK EXIST IN THE LIST
-                existingAsset.Investment += asset.Investment;
-                existingAsset.Unit += asset.Unit;
-                existingAsset.Summary.AddRange(asset.Summary);
+                // LIST IS NOT EMPTY
+                asset.AssetId = (_assets.Max(x => x.AssetId)) + 1;
+                _assets.Add(asset);
             }
         }
-
-        /// <summary>
-        /// THIS WILL HANDLE THE LOGIC TO REMOVE THE ASSET
-        /// </summary>
-        /// <param name="stockUnit"></param>
-        /// <param name="assetName"></param>
-        /// <param name="stockPrice"></param>
-        public static void RemoveAsset(int stockUnit, string assetName, double stockPrice)
+        else
         {
-            var asset = _assets.FirstOrDefault(x => x.Name == assetName);
-
-            if (stockUnit == asset.Unit)
-            {
-                _assets.Remove(asset);
-            }
-            else
-            {
-                var totalAmount = stockUnit * stockPrice;
-                asset.Unit -= stockUnit;
-                asset.Investment -= totalAmount;
-            }
+            // STOCK EXIST IN THE LIST
+            existingAsset.Investment += asset.Investment;
+            existingAsset.Unit += asset.Unit;
+            existingAsset.Summary.AddRange(asset.Summary);
         }
+
+        StoreAssets();
+    }
+
+    /// <summary>
+    /// THIS WILL HANDLE THE LOGIC TO REMOVE THE ASSET
+    /// </summary>
+    /// <param name="stockUnit"></param>
+    /// <param name="assetName"></param>
+    /// <param name="stockPrice"></param>
+    public static void RemoveAsset(int stockUnit, string assetName, double stockPrice)
+    {
+        LoadAssets();
+
+        var asset = _assets.FirstOrDefault(x => x.Name == assetName);
+
+        if (stockUnit == asset.Unit)
+        {
+            _assets.Remove(asset);
+        }
+        else
+        {
+            var totalAmount = stockUnit * stockPrice;
+            asset.Unit -= stockUnit;
+            asset.Investment -= totalAmount;
+        }
+
+        StoreAssets();
     }
 }
